0398171601dfad5e9224812522dc1f0bc5bc71f1
SimonKle <simon_klemm@web.de>
2020-07-23 14:11:05 +0200

    GameHost
    GameHost implemented

--- GameLib/GameHost.cpp
+++ GameLib/GameHost.cpp
@@ -4,10 +4,39 @@
 
 #include "GameHost.h"
 
-GameHost::GameHost(Ui &ui, std::vector<Player> players) {
+GameHost::GameHost(std::shared_ptr<Ui>& ui, std::vector<std::shared_ptr<Player>>& players)
+	:ui(ui)
+	,players(players)
+
+{
 
 }
 
-void GameHost::startGame() {
+void GameHost::startGame() 
+{
+	gameRules = std::make_shared<GameRules>();
 
+	int currentPlayerIndex = 0;
+
+	ui->drawGame();
+
+	while (true)  //Zeit für ein D D D D DUELL
+	{
+		const auto currentPlayer = players[currentPlayerIndex];
+		const auto nextPlayerIndex = (currentPlayerIndex + 1) %2;
+
+		currentPlayer->getGameDecision();
+
+		ui->drawGame(GameField);
+
+		const bool win = gameRules->didPlayerWin(currentPlayer);
+
+		if (win)
+		{
+			ui->showWinnerMessage(currentPlayer);
+			break;
+		}
+		currentPlayerIndex = (currentPlayerIndex + 1) % 2;
+	}
+	
 }
