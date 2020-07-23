0c9655d72d142eef87a3cf1042a2a5d05e08e450
Bennik2000 <bennik.ko@gmail.com>
2020-07-24 00:42:39 +0200

    Fix tests from player and player data separation

--- extern/googletest/testcases/MovePlayerDecision_test.cpp
+++ extern/googletest/testcases/MovePlayerDecision_test.cpp
@@ -1,6 +1,6 @@
-#include "gtest/gtest.h"
 #include "../../../GameLib/DummyPlayer.h"
 #include "../../../GameLib/GameField.h"
+#include "gtest/gtest.h"
 
 #include "../../../GameLib/Ui.h"
 
@@ -8,52 +8,52 @@
 
 TEST(MovePlayerDecisionTest, When_MakeInvalidMoveToRight_Assert_isValidMoveReturnsFalse)
 {
-	auto gameField = std::make_shared<GameField>();
-	std::shared_ptr<const Player> player = std::make_shared<const DummyPlayer>("Dummy", 0, BoardSides::Left);
+    auto gameField = std::make_shared<GameField>();
+    auto playerData = std::make_shared<PlayerData>("Dummy", 0, BoardSides::Right);
 
-	gameField->setPlayerPosition(player, std::make_shared<Coordinate>(0, 0));
-	gameField->setBorderBetweenCoordinates({0, 0}, {1, 0});
-	gameField->setBorderBetweenCoordinates({0, 0}, {0, 1});
-	gameField->setBorderBetweenCoordinates({1, 0}, {1, 1});
+    gameField->setPlayerPosition(playerData, std::make_shared<Coordinate>(0, 0));
+    gameField->setBorderBetweenCoordinates({0, 0}, {1, 0});
+    gameField->setBorderBetweenCoordinates({0, 0}, {0, 1});
+    gameField->setBorderBetweenCoordinates({1, 0}, {1, 1});
 
-	MovePlayerDecision decision(Direction::Right);
+    MovePlayerDecision decision(Direction::Right);
 
-	ASSERT_FALSE(decision.isValidMove(player, gameField));
+    ASSERT_FALSE(decision.isValidMove(playerData, gameField));
 }
 
 TEST(MovePlayerDecisionTest, When_ValidMoveToRight_Assert_isValidMoveReturnsTrue)
 {
-	auto gameField = std::make_shared<GameField>();
-	std::shared_ptr<const Player> player = std::make_shared<const DummyPlayer>("Dummy", 0, BoardSides::Left);
+    auto gameField = std::make_shared<GameField>();
+    auto playerData = std::make_shared<PlayerData>("Dummy", 0, BoardSides::Right);
 
-	gameField->setPlayerPosition(player, std::make_shared<Coordinate>(0, 0));
+    gameField->setPlayerPosition(playerData, std::make_shared<Coordinate>(0, 0));
 
-	MovePlayerDecision decision(Direction::Right);
+    MovePlayerDecision decision(Direction::Right);
 
-	ASSERT_TRUE(decision.isValidMove(player, gameField));
+    ASSERT_TRUE(decision.isValidMove(playerData, gameField));
 }
 
 TEST(MovePlayerDecisionTest, When_InValidMoveDown_Assert_isValidMoveReturnsFalse)
 {
-	auto gameField = std::make_shared<GameField>();
-	std::shared_ptr<const Player> player = std::make_shared<const DummyPlayer>("Dummy", 0, BoardSides::Left);
+    auto gameField = std::make_shared<GameField>();
+    auto playerData = std::make_shared<PlayerData>("Dummy", 0, BoardSides::Right);
 
-	gameField->setPlayerPosition(player, std::make_shared<Coordinate>(0, 0));
-	gameField->setBorderBetweenCoordinates({0, 0}, {0, 1});
+    gameField->setPlayerPosition(playerData, std::make_shared<Coordinate>(0, 0));
+    gameField->setBorderBetweenCoordinates({0, 0}, {0, 1});
 
-	MovePlayerDecision decision(Direction::Down);
+    MovePlayerDecision decision(Direction::Down);
 
-	ASSERT_FALSE(decision.isValidMove(player, gameField));
+    ASSERT_FALSE(decision.isValidMove(playerData, gameField));
 }
 
 TEST(MovePlayerDecisionTest, When_ValidMoveDown_Assert_isValidMoveReturnsTrue)
 {
-	auto gameField = std::make_shared<GameField>();
-	std::shared_ptr<const Player> player = std::make_shared<const DummyPlayer>("Dummy", 0, BoardSides::Left);
+    auto gameField = std::make_shared<GameField>();
+    auto playerData = std::make_shared<PlayerData>("Dummy", 0, BoardSides::Right);
 
-	gameField->setPlayerPosition(player, std::make_shared<Coordinate>(0, 0));
+    gameField->setPlayerPosition(playerData, std::make_shared<Coordinate>(0, 0));
 
-	MovePlayerDecision decision(Direction::Down);
+    MovePlayerDecision decision(Direction::Down);
 
-	ASSERT_TRUE(decision.isValidMove(player, gameField));
+    ASSERT_TRUE(decision.isValidMove(playerData, gameField));
 }
